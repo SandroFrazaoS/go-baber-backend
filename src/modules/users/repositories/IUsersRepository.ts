911f547b5b2f013d7602fc75b913dfbab9f74d60
Sandro Frazao Specht <sandro.s.frazao@gmail.com>
2021-02-02 14:27:03 -0300

    email 4

--- src/modules/users/repositories/IUsersRepository.ts
+++ src/modules/users/repositories/IUsersRepository.ts
@@ -1,7 +1,7 @@
 import User from '../infra/typeorm/entities/User';
 import ICreatUserDTO from '../dtos/ICreateUserDTO';
 
-export default interface IUserRepository {
+export default interface IUsersRepository {
     findById(id: string): Promise<User | undefined>;
     findByEmail(email: string): Promise<User | undefined>;
     create(data: ICreatUserDTO): Promise<User>;
